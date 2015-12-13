e75503690aed00bf793229e20695cb9f68a09d6c
Vinny Foreman <vinny@foreman-macbook.home>
2015-12-13 12:04:15 +0000

    font changes lato removed 100

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>FLIGHTDECK: own the skies</title>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="/css/style.css">
         <link rel="stylesheet" type="text/css" href="/css/sidebar.css">
