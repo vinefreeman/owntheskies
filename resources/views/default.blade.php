7b8c40618edbdd91c1677a84fe24e63d8c609c9f
Vinny Foreman <vinny.redhead@gmail.com>
2016-02-16 21:01:39 +0000

    added viewport declaration

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,6 +6,7 @@
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="/css/style.css">
         <link rel="stylesheet" type="text/css" href="/css/sidebar.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     </head>
     <body class="@yield('bodyClass')">
 
