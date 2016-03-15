8a8039ea01aaddb1aafe6202f892b97901ab6b33
Vinny Foreman <vinny.redhead@gmail.com>
2016-03-15 19:22:44 +0000

    added airlines feature and dashboard changes.  Cleaned up migrations to use a drop col rather than entire table on updates

--- database/migrations/2015_11_16_231305_create_airline_table.php
+++ database/migrations/2015_11_16_231305_create_airline_table.php
@@ -15,6 +15,7 @@ class CreateAirlineTable extends Migration
         Schema::create('airlines', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->string('name');
         });
     }
 
